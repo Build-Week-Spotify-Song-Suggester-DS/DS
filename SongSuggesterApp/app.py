ed2eb1ea87baf5ad47fc0438cfdb35829f60b562
daythan <90528859+daythan@users.noreply.github.com>
2022-02-03 08:11:37 -0600

    HTML and app

--- SongSuggesterApp/app.py
+++ SongSuggesterApp/app.py
@@ -1,27 +1,26 @@
+from flask import Flask, render_template, request
 
-'''stolen from ryans twitoff code refit'''
-
-def create_app():
-
-    app = Flask(__name__)
-
-    # configuration variable to our app
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Connect our database to the app object
-    DB.init_app(app)
-
-    @app.route("/")
-    def home_page():
-        # query for all users in the database
-        return render_template('base.html', title='Home', users=User.query.all())
-
-    @app.route("/reset")
-    def reset():
-        return 
-    
+app = Flask(__name__)
 
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    '''things within the home function will be run when
+    the page is loaded. The page is loaded via a "GET" request'''
 
-    
+    print('rendering the page')
+
+    '''When the form submits it will send a 'post' request
+    to this route, so anything inside of this if statement
+    will be run when the form is submitted'''
+
+    if request.method == 'POST':
+        print('submitting the form')
+        user_input = request.values['search']
+        print(user_input)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
